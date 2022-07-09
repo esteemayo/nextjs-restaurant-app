5bf862683a37944c37dff467bb4a01fabe9a8cc7
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-09 09:16:03 +0100

    modified

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -9,7 +9,7 @@ const handler = async (req, res) => {
   if (method === 'GET') {
     try {
       const orders = await Order.find();
-      res.status(200).json(orders);
+      res.status(200).json(orders).sort('-createdAt');
     } catch (err) {
       res.status(500).json({ message: err });
     }
