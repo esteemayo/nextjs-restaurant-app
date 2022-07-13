2a1ce6349e199266ce26161042d406635054ac23
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-13 16:07:16 +0100

    modified

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,15 @@
+import { ToastContainer } from 'react-toastify';
+
 import Footer from './Footer';
 import Navbar from './Navbar';
+
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <ToastContainer />
       {children}
       <Footer />
     </>
