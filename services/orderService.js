60e9aa4e541853043695cba0d1eca5548359e281
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-13 16:05:58 +0100

    modified

--- services/orderService.js
+++ services/orderService.js
@@ -9,3 +9,6 @@ export const getOrders = () => http.get(apiEndpoint);
 export const getOrder = (orderId) => http.get(orderUrl(orderId));
 
 export const createNewOrder = (order) => http.post(apiEndpoint, order);
+
+export const updateOrder = (orderId, order) =>
+  http.patch(orderUrl(orderId), order);
