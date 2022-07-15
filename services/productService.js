ff637a27ba22213c9907bf9661737d3209dbd5dd
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-15 21:46:47 +0100

    modified

--- services/productService.js
+++ services/productService.js
@@ -7,3 +7,10 @@ const productUrl = (productId) => `${apiEndpoint}/${productId}`;
 export const getProducts = () => http.get(apiEndpoint);
 
 export const getProduct = (productId) => http.get(productUrl(productId));
+
+export const createProduct = (product) => http.post(apiEndpoint, product);
+
+export const updateProduct = (productId, product) =>
+  http.patch(productUrl(productId), product);
+
+export const deleteProduct = (productId) => http.delete(productUrl(productId));
