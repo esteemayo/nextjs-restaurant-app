1f2746b64a15313b59ec62ca90bdacb08c9f7737
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-17 16:31:47 +0100

    modified

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
+import Add from '../components/Add';
 import Featured from '../components/Featured';
+import AddButton from '../components/AddButton';
 import PizzaList from '../components/PizzaList';
 import { getProducts } from '../services/productService';
 
 import styles from '../styles/Home.module.css';
 
-export default function Home({ products }) {
+export default function Home({ products, admin }) {
+  const [close, setClose] = useState(true);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,16 +20,26 @@ export default function Home({ products }) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Featured />
+      {admin && <AddButton setClose={setClose} />}
       <PizzaList products={products} />
+      {!close && <Add setClose={setClose} />}
     </div>
   );
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || '';
+  let admin = false;
+
+  if (myCookie.token === process.env.NEXT_PUBLIC_TOKEN) {
+    admin = true;
+  }
+
   const { data: products } = await getProducts();
   return {
     props: {
       products,
+      admin,
     },
   };
 };
