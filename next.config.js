667bc137bf203dfaedc159bf622d6e69f43da8de
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-17 16:29:49 +0100

    modified

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com']
+  },
+  env: {
+    NEXT_PUBLIC_TOKEN: process.env.NEXT_PUBLIC_TOKEN,
+    NEXT_PUBLIC_DEV_API_URL: process.env.NEXT_PUBLIC_DEV_API_URL,
+    NEXT_PUBLIC_PROD_API_URL: process.env.NEXT_PUBLIC_PROD_API_URL
+  }
 }
 
 module.exports = nextConfig
