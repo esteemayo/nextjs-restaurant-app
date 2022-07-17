c1c9316772b1e36e2c63659c85baaceb30df91ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-17 16:34:36 +0100

    added react redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+import { Provider } from 'react-redux';
+
+import store from '../features/store';
 import Layout from '../components/Layout';
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
