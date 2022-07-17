517da95bc4b4d4ab20eb45d93f6b62b934403948
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-17 16:33:48 +0100

    modified

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,15 +1,17 @@
+import Link from 'next/link';
 import Image from 'next/image';
+
 import styles from '../styles/PizzaCard.module.css';
 
-const PizzaCard = () => {
+const PizzaCard = ({ _id: id, img, desc, title, prices }) => {
   return (
     <div className={styles.container}>
-      <Image src='/img/pizza.png' alt='' width='500' height='500' />
-      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
-      <span className={styles.price}>$19.90</span>
-      <p className={styles.desc}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
+      <Link href={`/product/${id}`} passHref>
+        <Image src={img} alt='' width='500' height='500' />
+      </Link>
+      <h1 className={styles.title}>{title}</h1>
+      <span className={styles.price}>${prices[0]}</span>
+      <p className={styles.desc}>{desc}</p>
     </div>
   );
 };
