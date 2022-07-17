4750501a403ffd2348c23deff3d9d6474960f625
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-17 16:29:16 +0100

    first commit

---
+++ services/httpService.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+import logger from './logService';
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const NEXT_PUBLIC_DEV_API_URL = process.env.NEXT_PUBLIC_DEV_API_URL;
+const NEXT_PUBLIC_PROD_API_URL = process.env.NEXT_PUBLIC_PROD_API_URL;
+
+const API = axios.create({
+  baseURL: `${devEnv ? NEXT_PUBLIC_DEV_API_URL : NEXT_PUBLIC_PROD_API_URL}`,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+API.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    logger.log(error);
+    toast.error('An unexpected error occurred');
+  }
+
+  return Promise.reject(error);
+});
+
+const http = {
+  get: API.get,
+  put: API.put,
+  post: API.post,
+  patch: API.patch,
+  delete: API.delete,
+};
+
+export default http;
