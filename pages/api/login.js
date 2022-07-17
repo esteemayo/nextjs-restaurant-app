dbc1dd2b5c6745d1356c095818c50087f6597eae
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-17 16:34:54 +0100

    modified

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,18 +4,17 @@ const handler = (req, res) => {
   if (req.method === 'POST') {
     const { username, password } = req.body;
 
-    if (username === 'admin' && password === 'test1234') {
+    if (
+      username === process.env.ADMIN_USERNAME &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
       res.setHeader(
         'Set-Cookie',
-        cookie.serialize(
-          'token',
-          'wedfghbkjjkvkklkjjnhjkjf856t34ghe387gvgvhwtf2uygvwdnbvxy234t%*yi$@!yz$$%',
-          {
-            maxAge: 60 * 60 * 1000,
-            sameSite: 'strict',
-            path: '/',
-          }
-        )
+        cookie.serialize('token', process.env.TOKEN, {
+          maxAge: 60 * 60 * 1000,
+          sameSite: 'strict',
+          path: '/',
+        })
       );
 
       res.status(200).json('Successful');
