8b3f76eb5d5be4629950bcb3a56ed788555bf95d
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-18 02:50:11 +0100

    modified

--- pages/api/login.js
+++ pages/api/login.js
@@ -11,7 +11,7 @@ const handler = (req, res) => {
       res.setHeader(
         'Set-Cookie',
         cookie.serialize('token', process.env.TOKEN, {
-          maxAge: 60 * 60 * 1000,
+          maxAge: 60 * 60,
           sameSite: 'strict',
           path: '/',
         })
