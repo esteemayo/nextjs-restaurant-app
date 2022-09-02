c04c9b3240c86d6727f1ae1ce63608f3ca18d2a0
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-02 02:03:06 +0100

    modified

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -147,7 +147,7 @@ const Cart = () => {
                           })}
                         </span>
                       ) : (
-                        '_______'
+                        <span>&mdash;&mdash;&mdash;</span>
                       )}
                     </td>
                     <td>
