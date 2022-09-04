8b7212e006ac0a93de76b6f19807b94927a2c272
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-04 01:39:49 +0100

    added parseCookie

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,11 @@
-const excerpts = (str, count) => {
+import cookie from 'cookie';
+
+export const parseCookie = (req) =>
+  cookie.parse(req ? req.headers.cookie || '' : '');
+
+export const excerpts = (str, count) => {
   if (str.length > count) {
     str = str.slice(0, count) + '...';
   }
   return str;
 };
-
-export default excerpts;
