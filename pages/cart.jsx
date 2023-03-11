19cc5231ba99c0bec531c2f7e20e74385d0a71be
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-11 02:29:16 +0100

    fix: modify

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import {
   PayPalScriptProvider,
   PayPalButtons,
