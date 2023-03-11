60a09cb1077d6e0e5fbbacd6d562d9d3bcb20538
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-11 02:23:04 +0100

    fix: modify

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState } from 'react';
+import Link from 'next/link';
 import { useSelector } from 'react-redux';
 
 import styles from '@/styles/Navbar.module.css';
