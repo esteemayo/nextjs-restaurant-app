9920302b229db6fa7206ac54df25deae071aff97
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-11 02:19:29 +0100

    fix: change directory

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 
-import store from '@/features/store';
+import store from 'app/store';
 import Layout from '@/components/Layout';
 import BackToTop from '@/components/BackToTop';
 
