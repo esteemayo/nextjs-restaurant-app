20252c161d515969aff910e4b37d579d619d9865
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-11 02:20:38 +0100

    fix: modify

--- app/store.js
+++ app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import cartReducer from '../features/cart/cartSlice';
+import cartReducer from '@/features/cart/cartSlice';
 
 const store = configureStore({
   reducer: {
